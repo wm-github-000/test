7bd75f3aee05a67d0bed76ba1a81622005ac8879
wm <wm@example.com>
2022-04-14 10:18:19 +0800

    del

--- test.php
+++ test.php
@@ -7,4 +7,4 @@ $ly_email=$_POST["ly_email"];
 $sql="insert into liuyan(ly_title,ly_content,ly_time,ly_author,ly_email) values('".$ly_title."','".$ly_content."','".$ly_time."','".$ly_author."','".$ly_email."')";
 mysql_query($sql,$connec);
 echo("<script type='text/javascript'> alert('添加成功！');location.href='index.php';</script>");
-?>
+>
